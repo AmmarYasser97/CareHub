aa3546a29006bef9c0be748b5551b98abb39f49d
Ahmad Ganzouri <1500121@eng.asu.edu.eg>
2019-02-03 17:23:18 +0200

    Update forms.py

--- CareHub/accounts/forms.py
+++ CareHub/accounts/forms.py
@@ -7,3 +7,7 @@ class RegisterForm(forms.Form):
     password_repeat = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
     first_name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
     last_name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+
+class LoginForm(forms.Form):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
