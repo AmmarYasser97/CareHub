1d2b0654cfbd0025cc5093a503ddebd86e1230a6
Ahmad Ganzouri <1500121@eng.asu.edu.eg>
2019-02-03 17:24:05 +0200

    Update urls.py

--- CareHub/accounts/urls.py
+++ CareHub/accounts/urls.py
@@ -6,4 +6,7 @@ app_name = 'accounts'
 urlpatterns = [
    url(r'^signup/$', views.SignUp, name = 'signup'),
    url(r'^$', views.profile, name = 'profile'),
+   url(r'^signin/$', views.SignIn, name = 'signin'),
+   url(r'^signout/$', views.SignOut, name = 'signout'),
+   url(r'^Receptionist/$', views.Receptionist, name = 'receptionist'),
 ]
