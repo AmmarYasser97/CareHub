b9cba5d54a3e2a9bfa3085970808c3d1b86329d9
Ahmad Ganzouri <1500121@eng.asu.edu.eg>
2019-02-03 17:32:15 +0200

    Update urls.py

--- CareHub/CareHub/urls.py
+++ CareHub/CareHub/urls.py
@@ -17,11 +17,14 @@ from django.contrib import admin
 from django.conf.urls import url, include
 from . import views
 from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.home),
-    url(r'^accounts/', include('accounts.urls'))
-
+    url(r'^$', views.home, name = 'home'),
+    url(r'^accounts/', include('accounts.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
