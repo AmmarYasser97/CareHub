90f107be9d4179bed78b8f444fe8d2e36eb292fc
Ammar Yasser <ammaryasser085@gmail.com>
2019-02-06 17:37:03 +0200

    create doctor and service models

--- CareHub/Clinics/models.py
+++ CareHub/Clinics/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from Staff.models import Doctor
 
-# Create your models here.
+'''
+NOTE:
+this model is for both clinic and scan to save a lot of duplicate code and tables in database
+according to that approach the models.py of Scans application will be empty
+'''
+
+
+class Service(models.Model):
+    name = models.CharField(max_length=50)
+    start_time = models.TimeField(auto_now=False, auto_now_add=False)
+    end_time = models.TimeField(auto_now=False, auto_now_add=False)
+    no_appointments = models.PositiveSmallIntegerField()
+    type = models.CharField(max_length=1)
+    # type = 'C' /CLINIC or type = 'S' /SCAN
+    doctors = models.ManyToManyField(Doctor)
+    availability = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
--- CareHub/Staff/models.py
+++ CareHub/Staff/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
 
-# Create your models here.
+
+class Doctor(models.Model):
+    name = models.CharField(max_length=50)
+    expertise = models.CharField(max_length=50)
+    email = models.EmailField(max_length=70, blank=True, null=False, unique=True)
+    clinic = models.ForeignKey('Clinics.Service', null=True, on_delete=models.CASCADE)
+    rating = models.PositiveSmallIntegerField(validators=[MaxValueValidator(5)])
+    availability = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
