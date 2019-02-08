c827e7aad01e0f7c76c508af85e6c643df386ba5
khaledlooda <36304731+khaledlooda@users.noreply.github.com>
2019-02-08 01:10:57 +0100

    Update models.py

--- CareHub/Staff/models.py
+++ CareHub/Staff/models.py
@@ -6,6 +6,9 @@ from django.urls import reverse
 class Doctor(models.Model):
     name = models.CharField(max_length=50)
     # image = models.ImageField(upload_to=None, height_field=300, width_field=300,null=True)
+    #t3del mny(k)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    #end of t3del mny(k)
     expertise = models.CharField(max_length=50)
     email = models.EmailField(max_length=70, null=False, unique=True)
     rating = models.PositiveSmallIntegerField(validators=[MaxValueValidator(5)])
