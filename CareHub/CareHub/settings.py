12167defd4d7f8b971fa2b1e1e66ce3a2497ef4a
Ammaryasser <ammaryasser085@gmail.com>
2019-02-09 01:12:20 +0200

    update root app templates

--- CareHub/CareHub/settings.py
+++ CareHub/CareHub/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'media'),
+    os.path.join(BASE_DIR, 'static'),
 )
 #added by me to suit media(k)
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
