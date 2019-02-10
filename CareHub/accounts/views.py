a791577430f2fe7f0a551ec33d2c6f90e9134c43
Mariam El-sayed <36411139+mariamelsaayed@users.noreply.github.com>
2019-02-10 20:09:08 +0200

    Update views.py

--- CareHub/accounts/views.py
+++ CareHub/accounts/views.py
@@ -13,42 +13,7 @@ def Receptionist (request):
     return render(request, 'accounts/receptionist.html')
 
 def profile (request):
-    username = None
-    if request.user.is_authenticated():
-        username = request.user
-    show = Patient.objects.get(this=username)
-    context = {
-        'name': show.name,
-        'age': show.age,
-        'blood': show.blood,
-        'weight': show.weight,
-        'height': show.height,
-        'emergency': show.Emergency,
-        'contact': show.contact,
-        'donor': show.Organ_Donor,
-        'Allergies': show.Allergies,
-        'meds': show.Meds,
-        'operations': show.Operation,
-        'history': show.History,
-        'smoker': show.Smoker,
-        'athletic': show.Athletic,
-        'email': show.email
-    }
-    return render(request, 'accounts/profile.html', context)
-
-def profile_edit(request):
-    if request.method == 'POST':
-        form = forms.ProfileEdit(request.POST, request.FILES)
-        if form.is_valid():
-            instance = form.save(commit=False)
-            instance.patient = request.user
-            instance.save()
-            return redirect('profile')
-        else:
-            form = forms.ProfileEdit()
-    else:
-        form = forms.ProfileEdit()
-    return render(request, 'accounts/Profile-edit.html', {'forms': form})
+    return render(request, 'accounts/profile.html')
 
 
 def SignUp (request):
