6a7306de11615dd696175c1848f7295531d148af
Mariam El-sayed <36411139+mariamelsaayed@users.noreply.github.com>
2019-02-10 20:07:40 +0200

    Update models.py

--- CareHub/accounts/models.py
+++ CareHub/accounts/models.py
@@ -1,24 +1,3 @@
 from django.db import models
-from django.contrib.auth.models import User
 # Create your models here.
-
-
-class Patient(models.Model):
-    name = models.CharField(max_length=250, default=None)
-    age = models.IntegerField(default=None)
-    blood = models.CharField(max_length=3, default=None)
-    weight = models.IntegerField(default=None)
-    height = models.IntegerField(default=None)
-    Emergency = models.IntegerField(default=0)
-    Organ_Donor = models.CharField(max_length=3, default=None)
-    Allergies = models.TextField(default=None)
-    Meds = models.TextField(default=None)
-    Operation = models.TextField(default=None)
-    History = models.TextField(default=None)
-    contact = models.IntegerField(default=None)
-    Smoker = models.CharField(max_length=3, default=None)
-    Athletic = models.CharField(max_length=3, default=None)
-    email = models.CharField(max_length=250, default=None)
-    this = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
-
 
