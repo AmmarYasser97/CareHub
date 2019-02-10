5de043811e09ea3006fc761d7ecc5dd70b285569
MHesham98 <m-hesham-hassan@hotmail.com>
2019-02-11 00:54:17 +0200

    Patient Profile

---
+++ Profiles/Profile_App/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+from django.template import loader
+
+
+def patient(request):
+    template =loader.get_template(patient.html)
+    context = {
+
+
+    }
+    return HttpResponse(template.render(context, request))
---
+++ Profiles/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\hp\Documents\GitHub\SE2018G10\Profiles\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
