af07b82db68dffc41b33e9827b7953021d6f6d7e
MHesham98 <m-hesham-hassan@hotmail.com>
2019-02-11 01:35:42 +0200

    Patient model edits

--- Profiles/Profile_App/admin.py
+++ Profiles/Profile_App/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from Profile_App.models import Patient
+
+admin.site.register(Patient);
--- Profiles/Profile_App/models.py
+++ Profiles/Profile_App/models.py
@@ -3,8 +3,24 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Patient(models.Model):
+    Op = 'O Positive'
+    On = 'O Negative'
+    Ap = 'A Positive'
+    An = 'A Negative'
+    Bp = 'B Positive'
+    Bn = 'B Negative'
+    ABp = 'AB Positive'
+    ABn = 'AB Negative'
+
+    Blood_Types = ((Op, 'O+'),(On,'O-'),(Ap,'A+'),(An,'A-'),(Bp,'B+'),(Bn,'B-'),(ABp,'AB+'),(ABn,'AB-')
+    )
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image =  models.ImageField(default='defaultPatient.jpg',upload_to='patient_profile_pics')
+    Age = models.PositiveSmallIntegerField()
+    Blood_Type =models.CharField(max_length=11,choices=Blood_Types, default= Ap)
+    Weight =models.PositiveSmallIntegerField()
+    Height =models.PositiveSmallIntegerField()
+    Emergency_Contact =models.BigIntegerField()
 
-    def __str__(self):
+def __str__(self):
         return f'{self.user.username} Patient'
