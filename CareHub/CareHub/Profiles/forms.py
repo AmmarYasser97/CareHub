1062b2131bc7852265a0b71309aa53edc2c5253c
Mariam El-sayed <36411139+mariamelsaayed@users.noreply.github.com>
2019-02-10 20:24:54 +0200

    Delete forms.py

--- CareHub/CareHub/Profiles/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .import models
-
-
-class ProfileEdit(forms.ModelForm):
-    class Meta:
-        model = models.Patient
-        fields = ['name', 'age', 'blood', 'height', 'weight', 'Emergency', 'email', 'contact', 'Organ_Donor', 'Allergies', 'Athletic', 'Operation', 'Meds', 'Smoker', 'History']
