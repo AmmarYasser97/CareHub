f4bce217e2399afc1b649a2e80a15f895a9e17ea
Ammaryasser <ammaryasser085@gmail.com>
2019-02-12 13:54:35 +0200

    edit services type field

--- CareHub/Clinics/models.py
+++ CareHub/Clinics/models.py
@@ -10,13 +10,15 @@ class Service(models.Model):
     this model is for both clinic and scan to save a lot of duplicate code and tables in database
     according to that approach the models.py of Scans application will be empty
     '''
-
+    MY_CHOICES = (
+        ('a', 'Clinic'),
+        ('b', 'Scan'),
+    )
     name = models.CharField(max_length=50)
     start_time = models.TimeField(auto_now=False, auto_now_add=False)
     end_time = models.TimeField(auto_now=False, auto_now_add=False)
     no_appointments = models.PositiveSmallIntegerField(default=0)
-    type = models.CharField(max_length=1)
-    # type = 'C' for CLINIC or type = 'S' for SCAN 
+    type = models.CharField(max_length=7, choices=MY_CHOICES)
     doctors = models.ManyToManyField(Doctor)
     availability = models.BooleanField(default=False)
 
@@ -31,10 +33,9 @@ class Reservation(models.Model):
     Clinic = models.ForeignKey(Service, on_delete=models.CASCADE, default=None)
     Doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE, default=None)
     Booker = models.ForeignKey(User, on_delete=models.CASCADE)
-    Patient = models.CharField(max_length=50,default=str(Booker.name),)
+    Patient = models.CharField(max_length=50, default=str(Booker.name), )
     Time = models.TimeField(auto_now=False, auto_now_add=False)
     DateOfBook = models.DateField(auto_now_add=True)
-    
 
     def __str__(self):
         return self.Clinic.name
