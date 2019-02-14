3d527792afa6c3deb8fe81c7906d09c673f0048c
Salma Tarek <1500667@eng.asu.edu.eg>
2019-02-14 17:44:52 +0200

    Update forms.py

--- CareHub/accounts/forms.py
+++ CareHub/accounts/forms.py
@@ -7,6 +7,11 @@ class RegisterForm(forms.Form):
     password_repeat = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
     first_name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
     last_name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    Age = forms.IntegerField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    Height = forms.IntegerField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    Weight = forms.IntegerField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    Blood_Type = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    Contact = forms.IntegerField(widget=forms.TextInput(attrs={'class':'form-control'}))
 
 class LoginForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
