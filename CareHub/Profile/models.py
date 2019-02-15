d038c38cf085234ed6c6e39dd31e3004a2219c30
MHesham98 <m-hesham-hassan@hotmail.com>
2019-02-15 17:03:46 +0200

    Fixed Profile Picture Path

--- CareHub/Profile/models.py
+++ CareHub/Profile/models.py
@@ -20,7 +20,7 @@ class Patient(models.Model):
                    )
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(
-        default='defaultPatient.jpg', upload_to='     patient_profile_pics')
+        default='patient_profile_pics/defaultPatient.jpg', upload_to='     patient_profile_pics')
 
     # tkmlet User
 
@@ -32,7 +32,7 @@ class Patient(models.Model):
     Height = models.PositiveSmallIntegerField()
     Contact = models.BigIntegerField()
 
-    #Medical History
+    # Medical History
     Allergies = models.TextField(default="None")
     Organ_Donor = models.BooleanField(default=False)
     Medications = models.TextField(default="None")
@@ -43,9 +43,8 @@ class Patient(models.Model):
     Athletic = models.BooleanField(default=False)
     Smoker = models.BooleanField(default=False)
 
-
     def __str__(self):
-            return f'{self.user.username}'
+        return f'{self.user.username}'
 
     def get_absolute_url(self):
         return reverse('Profile:patient', kwargs={'name': self.pk})
